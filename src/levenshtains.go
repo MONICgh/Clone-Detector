b562602090c9f802426dc7192b34e6edee5d6984
Alyona Cherepanova <alyonacherepanova6954@gmail.com>
2023-04-12 18:53:59 +0300

    Add func Levenshtain's dist and tests

--- src/levenshtains.go
+++ src/levenshtains.go
@@ -1,8 +1,37 @@
 package main
 
-func Levenshtain(s1 string, s2 string) int {
-	n := len(s1)
-	m := len(s2)
+import "strings"
 
-	return Max(n, m) - Min(n, m)
+func Levenshtain(s1 string, s2 string) int {
+	n := len(strings.Split(s1, "\n"))
+	m := len(strings.Split(s2, "\n"))
+
+	var dp [][]int
+
+	for i := 0; i <= n; i++ {
+		var list []int
+		for j := 0; j <= m; j++ {
+			if i == 0 {
+				list = append(list, j)
+			} else if j == 0 {
+				list = append(list, i)
+			} else {
+				list = append(list, 0)
+			}
+		}
+		dp = append(dp, list)
+	}
+
+	for i, si := range strings.Split(s1, "\n") {
+		for j, sj := range strings.Split(s2, "\n") {
+			dp[i+1][j+1] = Min(dp[i][j+1], dp[i+1][j]) + 1
+			if si == sj {
+				dp[i+1][j+1] = Min(dp[i+1][j+1], dp[i][j])
+			} else {
+				dp[i+1][j+1] = Min(dp[i+1][j+1], dp[i][j]+1)
+			}
+		}
+	}
+
+	return dp[n][m]
 }
--- src/levenshtains_test.go
+++ src/levenshtains_test.go
@@ -10,9 +10,22 @@ func TestSimpleLevenshtain(t *testing.T) {
 
 	assert := assert.New(t)
 
-	s1 := "first \n second \n third \n end"
-	s2 := "second \n third \n end"
-	s3 := "first \n delete \n third \n end"
+	a := " a "
+	ab := " a \n b "
+	bb := " b \n b "
+
+	assert.Equal(Levenshtain(a, ab), 1, "dist("+a+", "+ab+"), must be 1")
+	assert.Equal(Levenshtain(a, bb), 2, "dist("+a+", "+bb+"), must be 2")
+	assert.Equal(Levenshtain(ab, bb), 1, "dist("+ab+", "+bb+"), must be 1")
+}
+
+func TestMediumLevenshtain(t *testing.T) {
+
+	assert := assert.New(t)
+
+	s1 := " first \n second \n third \n end "
+	s2 := " second \n third \n end "
+	s3 := " first \n delete \n third \n end "
 
 	assert.Equal(Levenshtain(s1, s2), 1, "dist("+s1+", "+s2+"), must be 1")
 	assert.Equal(Levenshtain(s1, s3), 1, "dist("+s1+", "+s3+"), must be 1")
